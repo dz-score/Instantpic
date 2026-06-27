3ede42fb756c24a1312c8445d29b326307d487fa
dz-score <sayan_dz@proton.me>
2026-06-27 03:54:43 +0200

    feat: enhance rerun script

--- rerun.sh
+++ rerun.sh
@@ -2,5 +2,17 @@
 cd frontend && npm run build
 cd ..
 uvicorn backend.main:app --host 0.0.0.0 --port 8000 &
-chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8000 &
+chromium-browser \
+  --kiosk \
+  --start-maximized \
+  --no-first-run \
+  --no-default-browser-check \
+  --disable-session-crashed-bubble \
+  --disable-infobars \
+  --disable-features=Translate,MediaRouter,OptimizationHints,AutofillServerCommunication \
+  --overscroll-history-navigation=0 \
+  --autoplay-policy=no-user-gesture-required \
+  --disable-pinch \
+   --noerrdialogs \
+    http://localhost:8000 &
 
