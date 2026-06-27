6297ed68a9c297041ec7e59f78f5cacb31fafbd6
dz-score <sayan_dz@proton.me>
2026-06-27 02:50:35 +0200

    feat(rerun): launch chromuim in script

--- rerun.sh
+++ rerun.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd frontend && npm run build
 cd ..
-uvicorn backend.main:app --host 0.0.0.0 --port 8000
+uvicorn backend.main:app --host 0.0.0.0 --port 8000 &
+chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8000 &
+
